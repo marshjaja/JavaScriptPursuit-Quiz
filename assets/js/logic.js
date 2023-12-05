bb97202b6650d05ee8e820fe5783ec326f412c50
marshjaja <114920895+marshjaja@users.noreply.github.com>
2023-12-05 14:54:59 +0000

    Add code to retrieve questions from questions datasetand implemented the initial functionality

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,8 @@
-const question = document.getElementById("questions");
+const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -59,6 +59,37 @@ const startQuiz = () => {
 	questionCounter = 0;
 	score = 0;
 	availableQuestions = [...questions];
-	console.log(availableQuestions);
+	getNextQuestion();
 };
+
+const getNextQuestion = () => {
+	if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+		//return "HOME"
+		return window.location.assign("/index.html");
+	}
+	questionCounter++;
+	const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+	currentQuestion = availableQuestions[questionIndex];
+	question.innerHTML = currentQuestion.question;
+
+	choices.forEach((choice) => {
+		const number = choice.dataset["number"];
+		choice.innerText = currentQuestion["choice" + number];
+	});
+
+	availableQuestions.splice(questionIndex, 1);
+	acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+	choice.addEventListener("click", (e) => {
+		console.log(e.target);
+		if (!acceptingAnswers) return;
+		acceptingAnswers = false;
+		const selectedChoice = e.target;
+		const selectedAnswer = selectedChoice.dataset["number"];
+		console.log(selectedAnswer);
+		getNextQuestion();
+	});
+});
 startQuiz();
